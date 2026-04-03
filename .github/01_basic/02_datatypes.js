1a898f10305ae0e5d9f4a91dab3aed175ad61187
ykuluriafzal16-netizen <ykuluri.afzal16@gmail.com>
2026-04-03 07:07:41 +0000

    datatypes

---
+++ .github/01_basic/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js code as newr version
+
+//alert (3+3) //we are using node js not browser
+/*console.log(3 + 3);// code readability should be high
+console.log("Afzal");
+
+let name = "Afzal"
+let age = 18
+let isLoggedIn = false
+
+console.table([name, age, isLoggedIn])
+
+//number => 2 to power of 53
+//bigint =>
+//string => "Afzal"
+//boolean => true/false
+//null 
+//undefined
+//symbol => unique
+
+//object*/
+
+console.log(typeof undefined);
+
