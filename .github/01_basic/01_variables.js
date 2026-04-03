1c0a94d898c993787f79dd04a30f86aa4a46a969
ykuluriafzal16-netizen <ykuluri.afzal16@gmail.com>
2026-04-03 07:07:18 +0000

    datatypes

---
+++ .github/01_basic/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 23415
+let accountEmail = "Afzal@google.com"
+var accountPassword = "Afzal@1234"
+accountCity = "Rayachoty"
+
+//console.log(accountId);
+console.table(accountId,accountEmail,accountPassword, accountCity)
