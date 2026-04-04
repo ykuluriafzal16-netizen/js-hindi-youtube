ede69cb49883d82a90556f96cf1c4da15cd72c86
ykuluriafzal16-netizen <ykuluri.afzal16@gmail.com>
2026-04-04 07:20:36 +0000

    convertion of Datatypes

---
+++ .github/01_basic/03_cinvertionOperations.js
@@ -0,0 +1,34 @@
+let score = "33abc"
+
+//console.log(typeof score);
+
+//let valueInNumber = Number (score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber);
+
+// 33 => it will give number as given variable
+//"33abc" =>Nan
+//true = 1 and false = 0
+
+let isLoggedIn = "Afzal"
+
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true; 0=> flase
+//"" => flase
+//"Afzal"=> true
+
+let someNumber = 564
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+
+
+
+
+
+
+
+
+
